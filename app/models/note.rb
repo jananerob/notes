bfcbac1c54937dcc326416a7ce3ef0ff53cda331
Jana Vorekova <janavorekova@Janas-MacBook-Pro.local>
2025-07-26 15:29:00 +0200

    Add new_method to note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,8 @@
 class Note < ApplicationRecord 
-=begin     attr_reader :title, :body
 
-    def initialize(title:, body:)
-        @title = title
-        @body = body
-    end 
-=end
+    def new_method
+        puts 'hello'
+    end
+
 end
   
