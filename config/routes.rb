a6dda56d85fd37d65a0391c69c6f2f5326b9cabe
Jana Vorekova <janavorekova@Janas-MacBook-Pro.local>
2025-07-26 16:50:37 +0200

    Add index and show for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
- 
 
   get 'notes', to: 'notes#index'
   get 'notes/:id', to: 'notes#show'
+
+  get 'items', to: 'items#index'
+  get 'items/:id', to: 'items#show'
 
   root 'pages#home'
   
